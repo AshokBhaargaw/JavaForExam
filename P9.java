b4a113414d96039c908716a40065085919a91f3e
Ashok Bhaargaw <ashokbhaargaw@gmail.com>
2024-02-26 18:28:54 -0800

    added P9

--- P9.java
+++ P9.java
@@ -1,4 +1,24 @@
-class P9{
+class circle{
+    int radius;
+    double area; 
 
+    void getArea(){
+        System.out.println("Enter radius of the circle:- ");
+        radius = Integer.parseInt(System.console().readLine());
+        area = 3.14 * radius * radius;
+    }
+
+    void printinfo(){
+        System.out.println("Radius of circle is - " + radius);
+        System.out.println("Area of the circle is:- " + area);
+    }
+}
+
+class P9{
+    public static void main(String[] args){
+        circle gola = new circle();
+        gola.getArea();
+        gola.printinfo();
+    }
     
 }
